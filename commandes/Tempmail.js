f5048902b8bf0fb2e8f002edd7c0b87f0578013c
Cytrak <149499127+Cytrak@users.noreply.github.com>
2024-04-21 21:23:33 +0300

    Tempmail.js

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
 france({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*FLASH-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*\n\n";
-    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
+    let z = '*FLASH-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *CYTRA-MD.*\n\n";
+    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com\n\n 5️⃣ https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://temp-mail.org/en/&ved=2ahUKEwiC_Pai89OFAxXMg_0HHUB3AW8QFnoECAYQAQ&usg=AOvVaw3z0rGMHgGrudTDmROkgggU';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+    var img = 'https://telegra.ph/file/6238d670a279057951ba5.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
